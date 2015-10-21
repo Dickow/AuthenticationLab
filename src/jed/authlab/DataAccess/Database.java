e95c875465e3af04f976ab5d07daea25793f3ea9
Dickow <Jeppedic@hotmail.com>
2015-10-21 18:18:30 +0200

    Added database package and sqlite JDBC

---
+++ src/jed/authlab/DataAccess/Database.java
@@ -0,0 +1,18 @@
+package jed.authlab.DataAccess;
+import java.sql.*;
+/**
+ * Created by Jeppe_000 on 21-10-2015.
+ */
+public class Database {
+    private Connection conn = null;
+    public Database(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:authenticationlab.db");
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened connection to database successfully");
+    }
+}
